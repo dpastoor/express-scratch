80895a88725df2004d79b7249f9f2cb3a9f5190d
Devin Pastoor <devin.pastoor@gmail.com>
2015-12-15 12:20:57 -0800

    complete step1 todo with es6

--- todos2/server.js
+++ todos2/server.js
@@ -1,5 +1,22 @@
 // TODO: create a basic server with express
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
+var express = require('express');
+var app = express();
 
 var jsonData = {count: 12, message: 'hey'};
+
+app.get('/', (req, res) =>
+  // res.sendFile takes an absolute path to a file and
+  // sets the mime type based on the file extension
+  res.sendFile(__dirname + '/index.html', (err) => {
+    if (err) {
+      res.status(600).send(err);
+    }
+  })
+);
+
+app.get('/data', (req, res) => res.json(jsonData))
+
+var port = 3000;
+app.listen(port, () => console.log('listening on http://localhost:' + port));
