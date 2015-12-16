43b3fd26a9443718eebbfee24d7b6a88696451c0
Devin Pastoor <devin.pastoor@gmail.com>
2015-12-15 19:34:31 -0800

    add notes about app.use

--- lions1/server/server.js
+++ lions1/server/server.js
@@ -6,10 +6,8 @@ var bodyParser = require('body-parser');
 var app = express();
 var _ = require('lodash');
 
-// express.static will serve everything
-// with in client as a static resource
-// also, it will server the index.html on the
-// root of that directory on a GET to '/'
+// express.static will serve everything with in client as a static resource
+// also, it will server the index.html on the root of that directory on a GET to '/'
 app.use(express.static('client'));
 
 // body parser makes it possible to post JSON to the server
@@ -17,11 +15,26 @@ app.use(express.static('client'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+/*
+what is going on with the app.use is basically
+var appMiddleware = []; (there is a stack) and every time we call app.use
+we are passing that function into the array (eg express.static('client'))
+so express.static is actually a function that returns another function
+var appMiddleware = [function(req, res, next){
+
+}, , , , ]
+and it runs them in order of registration every time we have a request come in
+
+This is GLOBAL middleware and it will run every request through this regardless
+of the endpoint it is trying to hit, before it gets appropriately routed
+*/
+
 
 var lions = [];
 var id = 0;
 
 // TODO: make the REST routes to perform CRUD on lions
+
 
 app.listen(3000);
 console.log('on port 3000');
